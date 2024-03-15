fe6a844a62ccb8e714256ed0230fedd2f24eb348
christian <155498959+christianoutleir@users.noreply.github.com>
2024-03-15 13:13:43 -0300

    Update script.js

--- .vscode/script.js
+++ .vscode/script.js
@@ -1,4 +1,3 @@
-
 function toggleMode() {
   const html = document.documentElement
   html.classList.toggle("light")
@@ -9,9 +8,9 @@ function toggleMode() {
   // substituir a imagem
   if (html.classList.contains("light")) {
     // se tiver light mode, adicionar a imagem light
-    img.setAttribute("src", "./avatar-light.png")
+    img.setAttribute("src", "./assets/avatar-light.png")
   } else {
     // set tiver sem light mode, manter a imagem normal
-    img.setAttribute("src", "./avatar2.png")
+    img.setAttribute("src", "./assets/avatar2.png")
   }
 }
